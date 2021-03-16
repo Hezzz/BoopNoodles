bb171a8b39f473356b6bf4a29c0b33b9f7c59d80
Hezzz <49769113+Hezzz@users.noreply.github.com>
2021-03-16 16:46:43 +0800

    Updated to PEP8 format

--- scheduler.py
+++ scheduler.py
@@ -1,35 +1,35 @@
 import csv
 from project import Project
+
+
 class Scheduler:
     schedule = []
 
     def __init__(self):
-      self.schedule = []
+        self.schedule = []
 
-        
-    def createSchedule(self):
+    def create_schedule(self):
         schedule = self.schedule
         
-        file = open('projects.txt','r',encoding='utf-8-sig')
+        file = open('projects.txt', 'r', encoding='utf-8-sig')
         table = csv.DictReader(file)
         line_count = 0
 
         for row in table:
-          if line_count == 0:
-            schedule.append(Project(row['id'],row['title'],row['size'],row['priority']))
+            if line_count == 0:
+                schedule.append(Project(row['id'],row['title'],row['size'],row['priority']))
         schedule.sort(key=lambda x: (x.get_priority(), x.get_size()))
         with open('schedule.txt', 'w') as f:
-          for project in schedule:
-              f.write("%s\n" % (project.get_id()+'          '+project.get_title()+'          '+project.get_size()+'          '+project.get_priority()))
+            for project in schedule:
+                f.write("%s\n" % (project.get_id()+'          '+project.get_title()+'          '+project.get_size()+'          '+project.get_priority()))
         
         file.close()
 
-    
-    def viewUpdatedSchedule(self):
+    def view_updated_schedule(self):
         try:
             with open('schedule.txt', 'r') as schedule:
                 print("ID          TITLE          SIZE        PRIO")
                 for project in schedule:
                     print(project)
-        except:
+        except FileNotFoundError:
             print("Please create a schedule first.")
