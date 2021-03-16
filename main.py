a463fb8c826b08c48925e88ec07a832525d39ea3
Hezzz <49769113+Hezzz@users.noreply.github.com>
2021-03-16 12:47:24 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Case Study (Python Implementation) in Structured Programming Languages
+Submitted to Hadji P. Tejuco (Course Adviser)
+FEU Tech
+Authors:
+    Rom Braveheart P. Leuterio (Dauntless Dev)
+    Hezekiah John V. Rizan (Hezzz)
+    Chrys Uoie A. Salazar (Memewtoo)
+"""
+
+
+def main():
+    """Main driver of the program."""
+    pass
+
+
+main()
