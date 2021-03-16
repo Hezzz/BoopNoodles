9473cec0b311d2ba196384d194e14644d7266c39
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2021-03-16 15:46:57 +0800

    Fixed and made scheduler with file io

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,12 @@
 import csv
 from project import Project
-
 class Scheduler:
     schedule = []
 
     def __init__(self):
       self.schedule = []
-    
+
+        
     def createSchedule(self):
         schedule = self.schedule
         
@@ -18,18 +18,18 @@ class Scheduler:
           if line_count == 0:
             schedule.append(Project(row['id'],row['title'],row['size'],row['priority']))
         schedule.sort(key=lambda x: (x.get_priority(), x.get_size()))
-
+        with open('schedule.txt', 'w') as f:
+          for project in schedule:
+              f.write("%s\n" % (project.get_id()+'          '+project.get_title()+'          '+project.get_size()+'          '+project.get_priority()))
         
         file.close()
 
     
     def viewUpdatedSchedule(self):
-      schedule = self.schedule
-      if not schedule:
-        print("Please create a schedule first.")
-      else:
-        print("ID          TITLE          SIZE        PRIO")
-        for project in schedule:
-          print((project.get_id()+'          '+project.get_title()+'          '+project.get_size()+'          '+project.get_priority()))
-
-          
+        try:
+            with open('schedule.txt', 'r') as schedule:
+                print("ID          TITLE          SIZE        PRIO")
+                for project in schedule:
+                    print(project)
+        except:
+            print("Please create a schedule first.")
