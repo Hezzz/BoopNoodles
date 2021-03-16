4d9c09784fa9c25ad086622df8e59bd56dd4a57b
Hezzz <49769113+Hezzz@users.noreply.github.com>
2021-03-16 12:52:13 +0800

    Update project.py

--- project.py
+++ project.py
@@ -2,23 +2,23 @@ class Project:
     """Project Class"""
 
     def __init__(self, project_id, title, size, priority):
-        self.project_id = project_id
-        self.title = title
-        self.size = size
-        self.priority = priority
+        self.__project_id = project_id
+        self.__title = title
+        self.__size = size
+        self.__priority = priority
 
     def get_id(self):
         """Returns the project id."""
-        return self.project_id
+        return self.__project_id
 
     def get_title(self):
         """Returns the project title."""
-        return self.title
+        return self.__title
 
     def get_size(self):
         """Returns the project size."""
-        return self.size
+        return self.__size
 
     def get_priority(self):
         """Returns the project priority"""
-        return self.priority
+        return self.__priority
