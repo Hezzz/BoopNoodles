743e7da5537c98c41b0c290a18c67b061f804e08
Yuwee Salazar <myuutuh@gmail.com>
2021-03-16 13:27:10 +0800

    menu() added

--- main.py
+++ main.py
@@ -8,10 +8,45 @@ Authors:
     Chrys Uoie A. Salazar (Memewtoo)
 """
 
+import sys
+
 
 def main():
     """Main driver of the program."""
-    pass
+    menu()
+
+def menu():
+    print (30*"-","MENU",30*"-")
+
+    print("""
+    a. Input Project Details
+    b. View Project
+    c. Schedule Projects
+    d. Get a Project
+    e. Exit
+    """)
+    print(67*"-")                  
+    choice = input("""Please enter your choice: """).lower()
+    
+    if choice == "a":
+        #input_project()
+        pass
+    elif choice == "b":
+        #view_project()
+        pass
+    elif choice == "c":
+        #schedule_project()
+        pass
+    elif choice == "d":
+        #getproject()
+        pass
+    elif choice == "e":
+        print("\nYou have exited.....")
+        sys.exit
+    else:
+        print("\nPlease select letter in the choices only.")
+        print("Try again.")
+        menu()
 
 
 main()
